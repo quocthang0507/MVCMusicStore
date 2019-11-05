d48282c6f4429fca4382d00f7782c29c03c29085
La Quốc Thắng <quocthang0507@gmail.com>
2019-11-05 11:17:18 +0700

    Update project

---
+++ MVCMusicStore/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+		// GET: Error
+		public ViewResult Index()
+		{
+			return View("Error");
+		}
+
+		// GET: NotFound
+		public ViewResult NotFound()
+		{
+			Response.StatusCode = 404;  //you may want to set this to 200
+			return View("NotFound");
+		}
+	}
+}
